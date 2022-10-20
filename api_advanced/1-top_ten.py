2518dcd91eee17b2f6ccd19794791c1b439dd5f0
gichuri <m.ndungu@alustudent.com>
2022-10-20 18:58:32 +0300

    trial

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-'''
+"""
 python script to fetch the titles of hottest 10 posts on a subreddit
 
-'''
+"""
 
 
 import requests
 
 
 def top_ten(subreddit):
-    ''' 
+    """
     define the url and the query
-    '''
+    """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10&t=all".format(subreddit)
 
-    '''
+    """
     make the request
-    '''
+    """
 
-    response = requests.get(url, headers = {'user-agent': 'youtbot'}).json()
+    response = requests.get(url, headers={"user-agent": "youtbot"}).json()
 
-    '''
+    """
     sieve through the JSON using a for loop
-    '''
+    """
     try:
         posts = []
 
-        for post in response['data']['children']:
-            x = post['data']['title']
+        for post in response["data"]["children"]:
+            x = post["data"]["title"]
             posts.append(x)
-        
-        print(posts)
-    except: 
-        print(None) 
 
+        print(posts)
+    except:
+        print(None)
